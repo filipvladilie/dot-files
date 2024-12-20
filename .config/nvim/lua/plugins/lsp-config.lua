e90bd9ba77a5b601fbae7fd044d2cb2e566e58b6
Vlad-Ilie Filip <vlad-iliefilip@Vlad-Ilies-MacBook-Pro.local>
2024-12-20 22:12:21 +0200

    initial dotfile commit nvim

---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+
+      -- Go
+      lspconfig.gopls.setup({
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      })
+
+      -- Swift
+      lspconfig.sourcekit.setup({
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
---
+++ .config/nvim/lua/plugins/mini-vim.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'echasnovski/mini.nvim',
+    config = function()
+      local statusline = require 'mini.statusline'
+      statusline.setup { use_icons = true }
+
+      local pairs = require 'mini.pairs'
+      pairs.setup()
+    end
+  }
+}
